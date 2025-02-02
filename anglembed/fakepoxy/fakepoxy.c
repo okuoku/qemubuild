3e2134816af3a7727702809068970cef84e64c35
okuoku <mjt@cltn.org>
2025-02-02 17:04:42 +0900

    fakepoxy: Implement extension detection

--- anglembed/fakepoxy/fakepoxy.c
+++ anglembed/fakepoxy/fakepoxy.c
@@ -1,6 +1,15 @@
 #include <epoxy/egl.h>
 #include <epoxy/gl.h>
 
+#include <stdio.h>
+#include <string.h>
+
+void
+fakepoxy__unimpl(const char* func){
+    fprintf(stderr, "FAKEPOXY: Unimpl [%s]\n", func);
+    return;
+}
+
 /* Fakepoxy */
 bool
 epoxy_is_desktop_gl(void){
@@ -10,17 +19,60 @@ epoxy_is_desktop_gl(void){
 int 
 epoxy_gl_version(void){
     /* FIXME: Implement this */
-    return 32;
+    return 31;
 }
 
 bool 
 epoxy_has_egl_extension(EGLDisplay dpy, const char *extension){
     /* FIXME: Implement this */
-    return true;
+    fprintf(stderr, "FAKEPOXY: EGL extension [%s] => false (hardcoded)\n",
+            extension);
+    return false;
+}
+
+static int
+tknlen(const char* tkn){
+    int res = 0;
+
+    while((*tkn != ' ') && (*tkn != 0)){
+        tkn++;
+        res++;
+    }
+
+    return res;
 }
 
 bool 
 epoxy_has_gl_extension(const char *extension){
-    /* FIXME: Implement this */
-    return true;
+    const int tgtlen = strnlen(extension, 255);
+    const char* lis = (const char*)glGetString(GL_EXTENSIONS);
+    const char* p = lis;
+    bool res = false;
+
+    if(! lis){
+        fprintf(stderr, "FAKEPOXY: No extensions?\n");
+        return false;
+    }
+
+    while(1){
+        //fprintf(stderr, "FAKEPOXY: strstr %s\n", p);
+        p = strstr(p, extension);
+        if(!p){
+            break;
+        }
+        const int len = tknlen(p);
+        if(len == tgtlen){
+            res = true;
+            break;
+        }
+        p += len;
+    }
+
+    if(res){
+        fprintf(stderr, "FAKEPOXY: GL extension [%s] => true\n", extension);
+    }else{
+        fprintf(stderr, "FAKEPOXY: GL extension [%s] => false\n", extension);
+    }
+
+    return res;
 }
